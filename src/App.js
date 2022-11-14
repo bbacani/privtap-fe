a10a11db3b4a7051be64674da3b6f279775b1df9
Vedran Hernaus <>
2022-11-14 13:22:37 +0100

    added google login button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import GoogleLogin from "react-google-login";
 
 function App() {
+    const handleFailure = (result) => {
+        console.log(result);
+        alert(result);
+    };
+
+    const handleLogin = (googleData) => {
+        console.log(googleData);
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <p>
-                    Hello!
-                </p>
+                <div className="loginContainer">
+                    <p>
+                        Hello!
+                    </p>
+                    <div>
+                        <GoogleLogin
+                            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                            buttonText="Log in with Google"
+                            onSuccess={handleLogin}
+                            onFailure={handleFailure}
+                            cookiePolicy={'single_host_origin'}
+                        ></GoogleLogin>
+                    </div>
+                </div>
             </header>
         </div>
     );
