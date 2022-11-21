d0534cea0dcede5e547d618e9064f7a0fa11481c
JackieJoker <danilocastiglia@hotmail.it>
2022-11-21 12:30:29 +0100

    Improved CreateAutomation, now it uses react-bootstrap

--- src/components/CreateAutomation/CreateAutomation.js
+++ src/components/CreateAutomation/CreateAutomation.js
@@ -1,81 +1,92 @@
 import React from "react";
 import "./CreateAutomation.css";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 function CreateAutomation() {
 
     return (
         <div>
             <h1>New automation</h1>
-            <h3>At PrivTAP we like to call this feature: If this then that</h3>
-            <br/><br/>
-            <div className="row">
-                <div className="column">
-                    <div className="triggerActionContainer">
-                        <h2>Trigger</h2>
-                        <div>
-                            <span>Platform:</span>
-                            <select name="cars" id="cars">
-                                <option value="volvo">Volvo</option>
-                                <option value="saab">Saab</option>
-                                <option value="mercedes">Mercedes</option>
-                                <option value="audi">Audi</option>
-                            </select>
-                        </div>
-                        <div>
-                            <span>Event:</span>
-                            <select name="cars" id="cars">
-                                <option value="volvo">Volvo</option>
-                                <option value="saab">Saab</option>
-                                <option value="mercedes">Mercedes</option>
-                                <option value="audi">Audi</option>
-                            </select>
-                        </div>
-                        <br/><br/>
-                        <h3>Which data would You like to get from this trigger?</h3>
-                        <div>
-                            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-                            <label htmlFor="vehicle1"> I have a bike</label><br/>
-                            <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
-                            <label htmlFor="vehicle2"> I have a car</label><br/>
-                            <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat"/>
-                            <label htmlFor="vehicle3"> I have a boat</label><br/>
-                        </div>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="triggerActionContainer">
-                        <h2>Action</h2>
-                        <div>
-                            <span>Platform:</span>
-                            <select name="cars" id="cars">
-                                <option value="volvo">Volvo</option>
-                                <option value="saab">Saab</option>
-                                <option value="mercedes">Mercedes</option>
-                                <option value="audi">Audi</option>
-                            </select>
-                        </div>
-                        <div>
-                            <span>Event:</span>
-                            <select name="cars" id="cars">
-                                <option value="volvo">Volvo</option>
-                                <option value="saab">Saab</option>
-                                <option value="mercedes">Mercedes</option>
-                                <option value="audi">Audi</option>
-                            </select>
-                        </div>
-                        <br/><br/>
-                        <h3>Which data would You like to share with this action?</h3>
-                        <div>
-                            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-                            <label htmlFor="vehicle1"> I have a bike</label><br/>
-                            <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
-                            <label htmlFor="vehicle2"> I have a car</label><br/>
-                            <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat"/>
-                            <label htmlFor="vehicle3"> I have a boat</label><br/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col md className="m-5">
+                        <h1 className="mb-5">Trigger</h1>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formTriggerPlatform">
+                                <Form.Label>Platform</Form.Label>
+                                <Form.Select aria-label="Select the platform">
+                                    <option>Select the platform</option>
+                                    <option value="1">Facebook</option>
+                                    <option value="2">Twitter</option>
+                                    <option value="3">Gmail</option>
+                                </Form.Select>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formTriggerEvent">
+                                <Form.Label>Event</Form.Label>
+                                <Form.Select aria-label="Select the event">
+                                    <option>Select the event</option>
+                                    <option value="1">A new post is added</option>
+                                    <option value="2">An email arrived</option>
+                                    <option value="3">Something</option>
+                                </Form.Select>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3">
+                                <Form.Text>Which data would You like to get from this trigger?</Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formTriggerPrivacy">
+                                <Form.Check type="checkbox" label="Name" />
+                                <Form.Check type="checkbox" label="Email" />
+                                <Form.Check type="checkbox" label="Content of the post" />
+                                <Form.Check type="checkbox" label="Location" />
+                                <Form.Check type="checkbox" label="Images" />
+                            </Form.Group>
+                        </Form>
+                    </Col>
+
+                    <Col md className="m-5">
+                        <h1 className="mb-5">Action</h1>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formTriggerPlatform">
+                                <Form.Label>Platform</Form.Label>
+                                <Form.Select aria-label="Select the platform">
+                                    <option>Select the platform</option>
+                                    <option value="1">Facebook</option>
+                                    <option value="2">Twitter</option>
+                                    <option value="3">Gmail</option>
+                                </Form.Select>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formTriggerEvent">
+                                <Form.Label>Event</Form.Label>
+                                <Form.Select aria-label="Select the event">
+                                    <option>Select the event</option>
+                                    <option value="1">A new post is added</option>
+                                    <option value="2">An email arrived</option>
+                                    <option value="3">Something</option>
+                                </Form.Select>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3">
+                                <Form.Text>Which data would You like to share with this action?</Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formTriggerPrivacy">
+                                <Form.Check type="checkbox" label="Name" />
+                                <Form.Check type="checkbox" label="Email" />
+                                <Form.Check type="checkbox" label="Content of the post" />
+                                <Form.Check type="checkbox" label="Location" />
+                                <Form.Check type="checkbox" label="Images" />
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                </Row>
+                <Button variant="primary" type="submit">
+                    Add automation
+                </Button>
+            </Container>
         </div>
     );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
     return <h1>This is home page.</h1>;
