e29a20b12889158230dfb67d828f49e3ca1eade0
Vedran Hernaus <>
2022-11-26 21:33:32 +0100

    updated header and homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import "./Home.css"
 
 function Home() {
     return (
-        <h1>This is home page.</h1>
+        <div>
+            <div>
+                <h3 className="title">Your automations</h3>
+                <div className="grid-container">
+                    <div className="grid-item">when TRIGGER then ACTION</div>
+                    <div className="grid-item">when TRIGGER then ACTION</div>
+                    <div className="grid-item">when TRIGGER then ACTION</div>
+                    <div className="grid-item">when TRIGGER then ACTION</div>
+                    <div className="grid-item">when TRIGGER then ACTION</div>
+                    <div className="grid-item">when TRIGGER then ACTION</div>
+                    <div className="grid-item">when TRIGGER then ACTION</div>
+                    <div className="grid-item">when TRIGGER then ACTION</div>
+                    <div className="grid-item">when TRIGGER then ACTION</div>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -5,11 +5,17 @@ import {Container, Nav, Navbar} from "react-bootstrap";
 function Header() {
 
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="light" className="header">
             <Container>
                 <Navbar.Brand href="/">
-                    PrivTap
+                    <h4 className="brand">privTAP</h4>
                 </Navbar.Brand>
+
+                <Nav className="justify-content-center">
+                    <Nav.Link href="/createAutomation">
+                        <h4 className="createAutomation">Create automation</h4>
+                    </Nav.Link>
+                </Nav>
 
                 <Nav className="justify-content-end">
                     <Nav.Link href="/login">Login</Nav.Link>
