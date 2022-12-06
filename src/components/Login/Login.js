9d13ce895979ace35a9996416d17ae0161182c83
JackieJoker <danilocastiglia@hotmail.it>
2022-12-06 11:53:23 +0100

    Login design renewed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,36 +1,55 @@
 import React from "react";
 import "./Login.css";
 import googleLogo from "../Login/img/google-logo.png"
+import artist from "../Login/img/Artist.png"
+import bubble from "../Login/img/chat-bubble.png"
 import {GOOGLE_AUTH_URL} from "../../config/constants";
 import {Button, Col, Container, Image, Row} from "react-bootstrap";
 
 function Login() {
     return (
-        <Container fluid className="p-3 my-5">
-
-            <Row>
-
-                <Col col='10' md='6'>
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone" />
-                </Col>
-
-                <Col col='4' md='6'>
-                    <div className='d-flex flex-row mt-2'>
-                        <span className="h1 fw-bold mb-0">PrivTap</span>
+        <div>
+            <div className="box">
+                <Container>
+                    <Col className="justify-content-md-center">
+                        <Row>
+                            <h2 align="center">
+                                Sign in to
+                            </h2>
+                        </Row>
+                        <Row>
+                            <h1 align="center">
+                                privTAP
+                            </h1>
+                        </Row>
+                        <Row>
+                            <h5 align="center" className="grey-text">
+                                Login to find back your automations.
+                            </h5>
+                        </Row>
+                        <Row>
+                            <Button className="mt-4" href={GOOGLE_AUTH_URL} size="lg" style={{backgroundColor: '#222222'}}>
+                                <Image className="googleButton" src={googleLogo} alt="Google"/>
+                                Sign in with Google
+                            </Button>
+                        </Row>
+                    </Col>
+                </Container>
+            </div>
+            <div className="bottom-centered">
+                <div className="artist-bubble">
+                    <div className="chat-bubble">
+                        <Image className="bubble-img" src={bubble} />
+                        <div className="centered-text">
+                            <center><p>Did you miss your automations?</p></center>
+                        </div>
                     </div>
-
-                    <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px'}}>Sign into your account</h5>
-
-                    <Button href={GOOGLE_AUTH_URL} className="  w-100" size="lg" style={{backgroundColor: '#3b5998'}}>
-                        <Image  fluid src={googleLogo} alt="Google"/>
-                        Log in with Google
-                    </Button>
-
-                </Col>
-
-            </Row>
-
-        </Container>
+                    <div className="artist-div">
+                        <Image className="artist" src={artist} />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
