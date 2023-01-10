606f0e8d1f5c25221ba975a3c2b2ec8ac4fd7485
JackieJoker <danilocastiglia@hotmail.it>
2023-01-10 11:12:56 +0100

    Platform Scopes page integrated

--- src/components/common/PlatformCard.js
+++ src/components/common/PlatformCard.js
@@ -2,8 +2,9 @@ import {Col} from "react-bootstrap";
 import React, {useEffect, useState} from "react";
 import "../Profile/Profile.css"
 import {service} from "../../service/ApiService";
+import {useNavigate} from "react-router-dom";
 export default function PlatformCard(props) {
-
+    const navigate = useNavigate();
     const [platform, setPlatform] = useState();
 
     useEffect(() => {
@@ -15,12 +16,15 @@ export default function PlatformCard(props) {
         getPlatformByName().then();
     }, []);
 
+    function handleClick() {
+        navigate('/scopes/' + props.name)
+    }
 
     return (
 
         <Col align="center" className="mb-5 mx-2 grid-item"
              style={{backgroundColor: platform?.color}}>
-            <div>
+            <div className="clickHover" onClick={handleClick}>
                 <h4 className="cardTitle text-uppercase">
                     {props.name}
                 </h4>
